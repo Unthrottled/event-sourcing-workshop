616aee5753c234e1af6d68502a0ad7b0dc5bffdf
DATBOI <alexsimons9999@gmail.com>
2018-10-11 18:41:07 -0500

    One blocking reactive mongo repository ._.

--- web-service/src/main/java/io/acari/event/source/config/WebConfig.java
+++ web-service/src/main/java/io/acari/event/source/config/WebConfig.java
@@ -1,9 +1,18 @@
 package io.acari.event.source.config;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
 @Configuration
 @EnableWebFlux
 public class WebConfig {
+
+    @Bean
+    public ObjectMapper objectMapper(){
+        return new ObjectMapper()
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }
 }
---
+++ web-service/src/main/java/io/acari/event/source/repository/PodMemberRepository.java
@@ -0,0 +1,60 @@
+package io.acari.event.source.repository;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.acari.springwebflux.models.Event;
+import org.bson.Document;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+@Service
+public class PodMemberRepository {
+    private final ReactiveMongoTemplate reactiveMongoTemplate;
+    private final ObjectMapper objectMapper;
+
+    public PodMemberRepository(ReactiveMongoTemplate reactiveMongoTemplate, ObjectMapper objectMapper) {
+        this.reactiveMongoTemplate = reactiveMongoTemplate;
+        this.objectMapper = objectMapper;
+    }
+
+    public Stream<Event> fetchPodMemberEventStream(String podMemberIdentifier){
+        return reactiveMongoTemplate.find(Query.query(Criteria.where("id")
+                .is(podMemberIdentifier)), Document.class, "podMemberEvents")
+                .map(document -> (List<Document>) document.get("events"))
+                .flatMap(documents ->
+                        Flux.fromIterable(documents)
+                .map(Document::toJson)
+                .flatMap(json -> {
+                    try {
+                        return Mono.just(objectMapper.readValue(json, Event.class));
+                    } catch (IOException e) {
+                        return Mono.error(e);
+                    }
+                })).toStream();
+
+    }
+
+    public Optional<Event> saveEvent(String podMemberIdentifier, Event event){
+        try {
+            return reactiveMongoTemplate.upsert(
+                    Query.query(Criteria.where("id").is(podMemberIdentifier)),
+            new Update().push("events", Document.parse(objectMapper.writeValueAsString(event))),//probably should figure out how to do this better.
+                    String.class, "podMemberEvents")
+                    .map(__-> event)
+                    .blockOptional();
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            return Optional.empty();
+        }
+    }
+}
