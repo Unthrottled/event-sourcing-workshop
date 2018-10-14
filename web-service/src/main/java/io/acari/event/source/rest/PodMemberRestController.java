98ee8a48ce699ba763566d108722fa2bffe211ce
DATBOI <alexsimons9999@gmail.com>
2018-10-14 11:08:19 -0500

    More wiring a controller

--- web-service/src/main/java/io/acari/event/source/rest/PodMemberRestController.java
+++ web-service/src/main/java/io/acari/event/source/rest/PodMemberRestController.java
@@ -1,8 +1,11 @@
 package io.acari.event.source.rest;
 
 import io.acari.event.source.handler.PodHandler;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.acari.event.source.models.Event;
+import io.acari.event.source.models.PersonalInformation;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/pod/member/{identifier}")
@@ -11,5 +14,15 @@ public class PodMemberRestController {
 
   public PodMemberRestController(PodHandler podHandler) {
     this.podHandler = podHandler;
+  }
+
+  @GetMapping("/information")
+  public Optional<PersonalInformation> getPodMemberInformation(@PathVariable("identifier") String id) {
+    return podHandler.projectPersonalInformation(id);
+  }
+
+  @PostMapping("/event")
+  public Optional<Event> savePodMemberEvent(@PathVariable("identifier") String id, @RequestBody Event eventToSave) {
+    return podHandler.savePodMemberEvent(id, eventToSave);
   }
 }
